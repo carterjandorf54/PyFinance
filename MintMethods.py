b51fe69710230584fae8449a676621a977fe0115
carterjandorf54 <carter@cjandorf.com>
2023-06-23 23:31:40 -0700

    Created MintMethods Class to contain contain all data

---
+++ MintMethods.py
@@ -0,0 +1,77 @@
+import csv
+import pandas as pd
+
+class MintMethods():
+
+    # Initialize Mint Methods with an Empty DataFrame
+    def __init__(self, path):
+        self.df = self.readCSV(path)
+        self.categories = self.getCategories(self.df)
+        self.date_range = self.getSimpleDateRange(self.df)
+
+    # Function to read in the CSV File and Create a Pandas DataFrame
+    def readCSV(self, path):
+        with open(path, mode="r") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            # Create the List of Dates for DataFrame
+            # Create the List of Money Spent for DataFrame
+            # Create the List of Categories for Spending
+            # Create the List of Account Names
+            dateList = []
+            moneyspent = []
+            category = []
+            account = []
+
+            for x in csv_reader:
+                dateList.append(x["Date"])
+                moneyspent.append(x["Amount"])
+                category.append(x["Category"])
+                account.append(x["Account Name"])
+
+            # Cast the amount spent to a float
+            for i in range(len(moneyspent)):
+                moneyspent[i] = float(moneyspent[i])
+            
+            # Add the List to a Dictionary
+            transactions = {"Date" : dateList, "Amount" : moneyspent, "Category" : category, "Account" : account}
+
+            # Create the Pandas DataFrame
+            df = pd.DataFrame(transactions)
+
+            # Return the Transactions Dictionary and DataFrame
+            return df
+
+    # Returns a simple dataframe with all fields from CSV File    
+    def easyReadCSV(self, path):
+        transactions = pd.read_csv(path)
+        return transactions
+    
+    # Returns a list of categories from the CSV File
+    def getCategories(self, df):
+        categories = []
+
+        for x in df["Category"]:
+            if x in categories:
+                pass
+            else:
+                categories.append(x)
+        
+        return categories
+    
+    def getSimpleDateRange(self, df):
+        dateRange = []
+        dates = []
+
+        for x in df["Date"]:
+            dates.append(x)
+
+        dateRange.append(dates[-1])
+        dateRange.append(dates[0])
+
+        return dateRange
+        
+    
+
+
+        
--- mint.py
+++ mint.py
@@ -1,92 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
+from MintMethods import MintMethods as mint
 
-# Function to read in the CSV File and Create a Pandas DataFrame
-# Returns a dictionary of all transactions and DataFrame with the transactions
-def readCSV(path):
-    with open(path, mode="r") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
+path = "assets\Carter_Finances_6_22_23.csv"
 
-        # Create the List of Dates for DataFrame
-        # Create the List of Money Spent for DataFrame
-        # Create the List of Categories for Spending
-        # Create the List of Account Names
-        dateList = []
-        moneyspent = []
-        category = []
-        account = []
-
-        for x in csv_reader:
-            dateList.append(x["Date"])
-            moneyspent.append(x["Amount"])
-            category.append(x["Category"])
-            account.append(x["Account Name"])
-
-        # Cast the amount spent to a float
-        for i in range(len(moneyspent)):
-            moneyspent[i] = float(moneyspent[i])
-        
-        # Add the List to a Dictionary
-        transactions = {"Date" : dateList, "Amount" : moneyspent, "Category" : category, "Account" : account}
-
-        # Create the Pandas DataFrame
-        df = pd.DataFrame(transactions)
-
-        # Return the Transactions Dictionary and DataFrame
-        return df
-
-# Returns a simple dataframe with all fields from CSV File    
-def easyReadCSV(path):
-    transactions = pd.read_csv(path)
-    return transactions
-
-# Returns a list of categories from the CSV File
-def getCategories(df):
-    categories = []
-
-    for x in df["Category"]:
-        if x in categories:
-            pass
-        else:
-            categories.append(x)
-    
-    return categories
-
-# Returns the date range from your last to first transaction
-def getComplexDateRange(df):
-    dateRange = []
-    dates = []
-
-    for x in df["Date"]:
-        dates.append(x)
-
-    temp_first = dates[-1].split("/")
-    first_date = f"{temp_first[2]}-{temp_first[0]}"
-    temp_last = dates[0].split("/")
-    last_date = f"{temp_last[2]}/{temp_last[0]}"
-
-    dateRange.append(first_date)
-    dateRange.append(last_date)
-
-    return dateRange
-
-def getSimpleDateRange(df):
-    dateRange = []
-    dates = []
-
-    for x in df["Date"]:
-        dates.append(x)
-
-    dateRange.append(dates[-1])
-    dateRange.append(dates[0])
-
-    return dateRange
-
-    
-df = readCSV("assets\Carter_Finances_6_22_23.csv")
-
+x = mint(path)
+print(x.date_range)  
+#print(x.df)
+'''
 x = getSimpleDateRange(df)
 y = getCategories(df)
 
@@ -106,7 +28,7 @@ ax.set_ylabel("Total Amount ($)")
 ax.tick_params(labelsize=8)
 
 plt.show()
-
+'''
 '''
 ts = pd.Series(newdf["Amount"], index=newdf["Date"])
 ts.plot()
